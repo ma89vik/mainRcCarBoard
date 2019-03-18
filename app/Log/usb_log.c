727106b876cf24c86458e9ce2ae31d3d2a5a32e6
MV <ma89vik@gmail.com>
2019-03-18 15:00:36 +0100

    Add mutex for usb log

--- app/Log/usb_log.c
+++ app/Log/usb_log.c
@@ -1,13 +1,15 @@
 #include "log.h"
 #include "usb_log.h"
 #include "usbd_cdc_if.h"
+#include "FreeRTOS.h"
+#include "semphr.h"
 
 static const LogApi_t api =
 {
     .write_string = usb_log_write,
 };
 
-
+static SemaphoreHandle_t xLogMutex;
 
 LogResult_t usb_log_init(Log_t *log)
 {
@@ -20,6 +22,7 @@ LogResult_t usb_log_init(Log_t *log)
     
     log->logApi = &api;
 
+    xLogMutex = xSemaphoreCreateMutex();
 
     CDC_Transmit_FS("Log init\n", 10);
     
@@ -29,8 +32,19 @@ LogResult_t usb_log_init(Log_t *log)
 
 LogResult_t usb_log_write(Log_t * log, const char *string, uint8_t len)
 {
+    if (xLogMutex != NULL)
+    {
+        const TickType_t xWaitPeriodMs = 10/portTICK_PERIOD_MS;
+        if(xSemaphoreTake(xLogMutex, xWaitPeriodMs) == pdTRUE)
+        {
+            CDC_Transmit_FS(string, len);
+        }
+        xSemaphoreGive(xLogMutex);
+        
+        return LOG_OK;
+    }
+
 
    
-   CDC_Transmit_FS(string, len);
 }
 
