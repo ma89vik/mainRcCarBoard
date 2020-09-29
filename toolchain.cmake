8adb62d71f638ec9cc39679e4ab592c54741ca84
ma89vik <ma89vik@gmail.com>
2020-09-29 21:35:58 +0800

    CI: checkout submodules

--- toolchain.cmake
+++ toolchain.cmake
@@ -2,14 +2,15 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR STM32F4)
 
 if(DEFINED ENV{TOOLCHAIN_PATH})
-  set(CMAKE_C_COMPILER $ENV{TOOLCHAIN_PATH}/bin/arm-none-eabi-gcc)
-  set(CMAKE_CXX_COMPILER $ENV{TOOLCHAIN_PATH}/bin/arm-none-eabi-g++)
-  set(CMAKE_ASM_COMPILER $ENV{TOOLCHAIN_PATH}/bin/arm-none-eabi-gcc)
+  set(CMAKE_C_COMPILER $ENV{TOOLCHAIN_PATH}/bin/arm-none-eabi-gcc.exe)
+  set(CMAKE_CXX_COMPILER $ENV{TOOLCHAIN_PATH}/bin/arm-none-eabi-g++.exe)
+  set(CMAKE_ASM_COMPILER $ENV{TOOLCHAIN_PATH}/bin/arm-none-eabi-gcc.exe)
 else()
   set(CMAKE_C_COMPILER arm-none-eabi-gcc)
   set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
   set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
 endif()
+
 
 set(CPU_FLAGS "-mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mno-thumb-interwork -mthumb -specs=nosys.specs")
 set(COMMON_FLAGS "-Wall -ffunction-sections -fdata-sections  -Wl,-Map=output.map ${CPU_FLAGS}")
